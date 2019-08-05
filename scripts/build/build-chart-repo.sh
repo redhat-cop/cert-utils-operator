07f1aace6fb78567536323cf7ea5a352e567d715
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2019-08-05 14:11:04 -0400

    fixed url

--- scripts/build/build-chart-repo.sh
+++ scripts/build/build-chart-repo.sh
@@ -2,10 +2,11 @@ echo '>> Building charts...'
 find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
   echo ">>> helm lint $chart"
   helm lint "$chart"
-  chart_name="$HELM_CHART_DEST/$chart"
+  chart_name=$HELM_CHART_DEST/"`basename "$chart"`"
+  #chart_name="$HELM_CHART_DEST/$chart"
   echo ">>> helm package -d $chart_name $chart"
   mkdir -p "$chart_name"
   helm package -d "$chart_name" "$chart"
 done
 echo '>>> helm repo index'
-helm repo index $HELM_CHART_DEST
+helm repo index --url https://$(dirname $GITHUB_PAGES_REPO).github.io/$(basename $GITHUB_PAGES_REPO) $HELM_CHART_DEST
