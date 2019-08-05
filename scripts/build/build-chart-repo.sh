2378eab63646e09d8fefd1e14f02b2335d5bb067
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2019-08-05 15:59:10 -0400

    added versions to helm templates

--- scripts/build/build-chart-repo.sh
+++ scripts/build/build-chart-repo.sh
@@ -1,12 +1,14 @@
 echo '>> Building charts...'
 find "$HELM_CHARTS_SOURCE" -mindepth 1 -maxdepth 1 -type d | while read chart; do
+  version=${TRAVIS_TAG} envsubst < $chart/values.yaml.tpl > $chart/values.yaml
+  version=${TRAVIS_TAG} envsubst < $chart/Chart.yaml.tpl  > $chart/Chart.yaml
+  #sed -i "s/latest/${TRAVIS_TAG}/g" $chart/values.yaml
   echo ">>> helm lint $chart"
   helm lint "$chart"
-  chart_name=$HELM_CHART_DEST/"`basename "$chart"`"
-  #chart_name="$HELM_CHART_DEST/$chart"
-  echo ">>> helm package -d $chart_name $chart"
-  mkdir -p "$chart_name"
-  helm package -d "$chart_name" "$chart"
+  chart_dest=$HELM_CHART_DEST/"`basename "$chart"`"
+  echo ">>> helm package -d $chart_dest $chart"
+  mkdir -p "$chart_dest"
+  helm package -d "$chart_dest" "$chart"
 done
-echo '>>> helm repo index'
+echo '>>>' "helm repo index --url https://$(dirname $GITHUB_PAGES_REPO).github.io/$(basename $GITHUB_PAGES_REPO) $HELM_CHART_DEST"
 helm repo index --url https://$(dirname $GITHUB_PAGES_REPO).github.io/$(basename $GITHUB_PAGES_REPO) $HELM_CHART_DEST
