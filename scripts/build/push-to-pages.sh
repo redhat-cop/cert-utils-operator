783c874ac327fe56a48d376f46b072ac41f54ecd
raffaelespazzoli <raffaele.spazzoli@gmail.com>
2019-08-05 15:08:09 -0400

    fixed git authentication

--- scripts/build/push-to-pages.sh
+++ scripts/build/push-to-pages.sh
@@ -1,5 +1,7 @@
 git -C $GITHUB_PAGES_DIR config user.email "$redhat-cop@users.noreply.github.com"
 git -C $GITHUB_PAGES_DIR config user.name $GIT_USERNAME
+git -C $GITHUB_PAGES_DIR config credential.helper 'store --file ~/.travis.git.credentials'
+echo https://$GIT_USERNAME:$GIT_PASSWORD@github.com >> ~/.travis.git.credentials
 git -C $GITHUB_PAGES_DIR add .
 git -C $GITHUB_PAGES_DIR status
 git -C $GITHUB_PAGES_DIR commit -m "Published by Travis"
